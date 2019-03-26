b4ae5d549b786aa351e171e8c91b4dd865f86c7a
Ryan O'Laughlin <ryanolaughlin97@gmail.com>
2019-03-25 19:24:50 -0600

    Fixed upload

---
+++ lab5/public/admin.js
@@ -0,0 +1,80 @@
+var app = new Vue({
+  el: '#admin',
+  data: {
+    title: "",
+    file: null,
+    editDescription: "",
+    uploadDescription: "",
+    addItem: null,
+    items: [],
+    findTitle: "",
+    findItem: null,
+  },
+  created() {
+    this.getItems();
+  },
+  computed: {
+    suggestions() {
+      return this.items.filter(item => item.title.toLowerCase().startsWith(this.findTitle.toLowerCase()));
+    }
+  },
+  methods: {
+    fileChanged(event) {
+      this.file = event.target.files[0]
+    },
+    selectItem(item) {
+      this.findTitle = "";
+      this.findItem = item;
+    },
+    async deleteItem(item) {
+      try {
+        let response = axios.delete("/api/items/" + item._id);
+        this.findItem = null;
+        this.getItems();
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async upload() {
+      try {
+        const formData = new FormData();
+        formData.append('photo', this.file, this.file.name)
+        let r1 = await axios.post('/api/photos', formData);
+        let r2 = await axios.post('/api/items', {
+          title: this.title,
+          path: r1.data.path,
+          description: this.uploadDescription,
+        });
+        this.addItem = r2.data;
+        this.getItems();
+        this.uploadDescription = "";
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async editItem(item) {
+      try {
+        let response = await axios.put("/api/items/" + item._id, {
+          title: this.findItem.title,
+          description: this.editDescription
+        });
+        this.findItem = null;
+        this.getItems();
+        this.editDescription = "";
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getItems() {
+      try {
+        let response = await axios.get("/api/items");
+        this.items = response.data;
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  }
+});
